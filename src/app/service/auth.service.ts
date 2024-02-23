84934f26433c4d573ff76bf6a122972a80b0fef4
LeandroFernandezRuhl <leandro.feruhl@gmail.com>
2024-02-22 22:15:06 -0300

    Remove port from API requests

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   }
 
   getUser(): Observable<User> {
-    return this.http.get<User>('https://betkick-api.leandroruhl.com:8080/api/user', {headers}, )
+    return this.http.get<User>('https://betkick-api.leandroruhl.com/api/user', {headers}, )
       .pipe(map((response: User) => {
           if (response !== null) {
             this.$authenticationState.next(true);
@@ -37,11 +37,11 @@ export class AuthService {
   }
 
   login(): void {
-    location.href = `${location.origin}${this.location.prepareExternalUrl('https://betkick-api.leandroruhl.com:8080/oauth2/authorization/okta')}`;
+    location.href = `${location.origin}${this.location.prepareExternalUrl('https://betkick-api.leandroruhl.com/oauth2/authorization/okta')}`;
   }
 
   logout(): void {
-    this.http.post('https://betkick-api.leandroruhl.com:8080/api/logout', {}, { withCredentials: true }).subscribe(
+    this.http.post('https://betkick-api.leandroruhl.com/api/logout', {}, { withCredentials: true }).subscribe(
       (response: any) => {
         // Redirect to the logout URL received from the server
         window.location.href = response.logoutUrl;
